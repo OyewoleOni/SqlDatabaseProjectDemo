97efb7aa0564700d61f6560435326984522a9fff
Oyewole Oni <ooni@eminenttechnology.com>
2020-01-27 19:06:20 +0100

    Add project files.

---
+++ SQLProject/dbo/Views/FullPerson.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+	AS 
+	select [p].[Id] AS PersonId, [p].[FirstName], [p].[LastName], 
+		   [a].[Id]  AS AddressId, [a].[StreetAddress], [a].[City], [a].[State], [a].[ZipCode]
+	from dbo.Person p
+	left join dbo.Address a on p.Id = a.PersonId
